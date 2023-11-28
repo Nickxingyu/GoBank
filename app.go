b7ad50b1f46a9771b791628dc3891e2b9393031e
Hero <nick19960723@gmail.com>
2023-11-28 21:13:51 +0800

    feat: connect postgres by gorm

--- app.go
+++ app.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"github.com/Nickxingyu/GoBank/internal/app"
+	"github.com/Nickxingyu/GoBank/internal/database"
 )
 
 func main() {
+	db := database.Postgres{
+		Host:     "localhost",
+		User:     "postgres",
+		Password: "admin",
+		DBname:   "GoBank",
+		Port:     "8080",
+		SSLmode:  "disable",
+	}
+	db.Conn()
+
 	server := app.NewApiServer(":3000")
 	server.Run()
 }
---
+++ internal/database/postgres.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Postgres struct {
+	Host     string
+	User     string
+	Password string
+	DBname   string
+	Port     string
+	SSLmode  string
+}
+
+func (p Postgres) getDsn() string {
+	return "host=" + p.Host + " user=" + p.User + " password=" + p.Password + " dbname=" + p.DBname + " port=" + p.Port + " sslmode=" + p.SSLmode
+}
+
+func (p Postgres) Conn() {
+	dsn := p.getDsn()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	DB = db
+}
