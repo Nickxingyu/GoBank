30e9e4d8779fffd9087bedc973a1bf1ed9f02dad
Hero <nick19960723@gmail.com>
2023-12-01 14:07:10 +0800

    feat: deleting an account by transaction

--- internal/model/account.go
+++ internal/model/account.go
@@ -42,10 +42,10 @@ func DeleteAccountByID(accountID uint) (*Account, error) {
 	var account Account
 
 	err := database.DB.Transaction(func(tx *gorm.DB) error {
-		if err := database.DB.Find(&account, accountID).Error; err != nil {
+		if err := tx.Find(&account, accountID).Error; err != nil {
 			return err
 		}
-		if err := database.DB.Delete(&account).Error; err != nil {
+		if err := tx.Delete(&account).Error; err != nil {
 			return err
 		}
 
